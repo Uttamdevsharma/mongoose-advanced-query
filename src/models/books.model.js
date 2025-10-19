a8850531bdbdee0615c60eaedddf2b5cbb113a2c
Uttam <uttamrohit4545@gmail.com>
2025-10-19 18:26:13 +0600

    feat : aggreagation

---
+++ src/models/books.model.js
@@ -0,0 +1,18 @@
+const {Schema,model}= require('mongoose');
+
+const bookSchema = new Schema({
+  title: String,
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Author"
+  },
+  price: Number,
+  sales: Number,
+  genre: String
+});
+
+const Book = model("Book", bookSchema);
+
+module.exports = {
+  Book
+};
--- src/routes/movies.router.js
+++ src/routes/movies.router.js
@@ -1,26 +1,104 @@
 const {Router} = require('express')
-const { Movie } = require('../models/movies.model')
+const {Movie} = require('../models/movies.model')
 const router = Router()
 
 
-
+// get router
 router.get("/", async(req,res) =>{
-    const res = await Movie.find();
+
+   
+
+    const response = await Movie.find({
+
+        //Comparison Operator - $eq.$ne
+        // title : {
+        //     $eq : "The Great Train Robbery"
+
+        // },
+        // type :{$ne : "movie"}
+
+        
+
+        // $gt , $gte , $in , $lt, $lte
+        // runtime : {$lt : 50}
+         
+        //nested fields
+        // 'awards.wins' : {$gte : 2}
+
+
+        // year : {$gte : 1900} ,
+        // runtime : {$lt : 50}
+
+        //$in
+        // cast : {
+        //     $nin :["George Barnes"]
+        // }
+
+
+
+        //logical operator -$and , $or , $not , $nor
+
+        // $or: [ //and same
+        //     {
+        //         // rated : {$eq : "accepted"},
+        //         rated :{$not : {$eq : "Approved" } },
+
+        //         cast : {$in : "John Wayne"}
+        //     },
+        //     {
+        //         type : {$eq : "movie"}
+        //     }
+        // ]
+
+
+
+        //Element Operator - $exists , $type
+
+        // runtime : {
+        //     $exists : true,
+        //     $type : "string"
+        // }
+
+
+
+
+
+
+        //evaluation operator
+        title: { $regex: "^t.*y$", $options: "i" }
+
+
+    });
+
+    
+
+    //pagination
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * limit;
+    const movies = await Movie.find({}).sort({runtime:-1}).limit(limit).skip(skip)
+    const totalMovies = await Movie.countDocuments();
+
 
     return res.status(200).json({
         success : true,
         message : "Movies Fetched Succesfully",
-        data: res
-    })
-
-
-    router.post("/add-movie" , async(req,res) => {
-        const movie = req.body;
-
-        const response = await Movie.create(req.body)
-        res.status(200).send({
-            message : "successfully created",
-            data : response
-        })
+        count : totalMovies || 0,
+        data: movies,
+        limit : Number(limit)
     })
 })
+
+
+//post router
+router.post("/add-movie" , async(req,res) => {
+    const movie = req.body;
+
+    const response = await Movie.create(movie)
+    res.status(200).send({
+        message : "successfully created",
+        data : response
+    })
+})
+
+module.exports = router
